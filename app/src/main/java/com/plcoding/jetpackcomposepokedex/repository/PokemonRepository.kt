fb1f82b241c0e495f5382ca3af3ebffd98345d6a
Hitesh Kuber <hiteshkuber@gmail.com>
2023-08-17 23:52:56 +0530

    Day 2

---
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/repository/PokemonRepository.kt
@@ -0,0 +1,31 @@
+package com.plcoding.jetpackcomposepokedex.repository
+
+import com.plcoding.jetpackcomposepokedex.data.remote.PokeAPI
+import com.plcoding.jetpackcomposepokedex.data.remote.responses.Pokemon
+import com.plcoding.jetpackcomposepokedex.data.remote.responses.PokemonList
+import com.plcoding.jetpackcomposepokedex.util.Resource
+import dagger.hilt.android.scopes.ActivityScoped
+import javax.inject.Inject
+
+@ActivityScoped
+class PokemonRepository @Inject constructor(private val api:PokeAPI) {
+
+    suspend fun getPokemonList(limit: Int, offset: Int): Resource<PokemonList> {
+        val response = try {
+            api.getPokemonList(limit, offset)
+        } catch (e: Exception) {
+            return Resource.Error("Error occurred")
+        }
+
+        return Resource.Success(response)
+    }
+    suspend fun getPokemonInfo(name: String): Resource<Pokemon> {
+        val response = try {
+            api.getPokemonInfo(name)
+        } catch (e: Exception) {
+            return Resource.Error("Error occurred")
+        }
+
+        return Resource.Success(response)
+    }
+}
